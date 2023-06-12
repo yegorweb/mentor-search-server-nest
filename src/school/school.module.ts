82dddfa91f4c7cc5696a983524044b7fc8ea63ff
Yegor Knyazev <egorknyazev10@gmail.com>
2023-06-12 12:35:33 +0400

    fixed school module

--- src/school/school.module.ts
+++ src/school/school.module.ts
@@ -1,10 +1,7 @@
-import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { MongooseModule } from '@nestjs/mongoose';
-import { AuthMiddleware } from 'src/middlewares/auth.middleware';
 import { TokenModule } from 'src/token/token.module';
 import SchoolModel from './models/school.model';
-import { SchoolClass, SchoolSchema } from './schemas/school.schema';
 import { SchoolController } from './school.controller';
 
 @Module({
@@ -15,10 +12,4 @@ import { SchoolController } from './school.controller';
   ],
   controllers: [SchoolController],
 })
-export class SchoolModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(AuthMiddleware)
-      .forRoutes('create', 'switch-to-active')
-  }
-}
+export class SchoolModule {}
