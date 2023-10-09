ba22186b96502a79316771c2637af66a4e830c65
Yegor Knyazev <egorknyazev10@gmail.com>
2023-10-09 17:20:42 +0400

    added func to check access to role

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -15,7 +15,7 @@ export class UserService {
     private RolesService: RolesService
   ) {}
 
-  hasAccess(roles: string[], user: User | UserFromClient) {
+  hasAccess(roles: string[], user: User | UserFromClient): boolean {
     return this.RolesService.isAdminOfSchool(roles, new mongoose.Types.ObjectId(user.school._id).toString()) ||
            this.RolesService.isAdminOfTown(roles, new mongoose.Types.ObjectId(user.school.town._id).toString()) ||
            this.RolesService.isGlobalAdmin(roles)
@@ -25,37 +25,49 @@ export class UserService {
     assigning_user_roles: string[], 
     old_subject_roles: string[],
     new_subject_roles: string[]
-  ) {
-    if (this.RolesService.isGlobalAdmin(assigning_user_roles))
-      return
-
+  ): Promise<void> {
     let pushed_roles = new_subject_roles.filter(role => !old_subject_roles.includes(role))
     let pulled_roles = old_subject_roles.filter(role => !new_subject_roles.includes(role))
 
-    if (!pushed_roles.every(role => assigning_user_roles.includes(role)))
-      throw ApiError.AccessDenied()
-
-    if (pulled_roles.some(role => role === 'global-admin'))
-      throw ApiError.AccessDenied()
-
     if (
-      this.RolesService.getTownIdsFromRoles(pulled_roles).length > 0 &&
-      !this.RolesService.isGlobalAdmin(assigning_user_roles)
+      !pushed_roles.every(role => this.hasAccessToRole(assigning_user_roles, role)) &&
+      !pulled_roles.every(role => this.hasAccessToRole(assigning_user_roles, role))
     )
       throw ApiError.AccessDenied()
+  }
 
-    if (this.RolesService.getSchoolIdsFromRoles(pulled_roles).length > 0) {
-      let administered_towns_ids = this.RolesService.getTownObjectIdsFromRoles(assigning_user_roles)
+  async hasAccessToRole(user_roles: string[], role: string): Promise<boolean> {
+    if (this.RolesService.isOwner([role]))
+      return false
+    
+    if (this.RolesService.isOwner(user_roles))
+      return true
 
-      if (administered_towns_ids.length === 0)
-        throw ApiError.AccessDenied()
+    if (this.RolesService.isGlobalAdmin([role]))
+      return false
 
-      for (let school_id in this.RolesService.getSchoolIdsFromRoles(pulled_roles)) {
-        let school = await this.SchoolModel.findById(school_id)
+    if (this.RolesService.isOwner([role]))
+      return false
+    
+    if (this.RolesService.getTownIdsFromRoles([role]).length)
+      return this.RolesService.isGlobalAdmin(user_roles)
+    
+    if (this.RolesService.getSchoolIdsFromRoles([role]).length && !user_roles.includes(role)) {
+      let administered_towns_ids = this.RolesService.getTownObjectIdsFromRoles(user_roles)
+      if (!administered_towns_ids.length)
+        return false
 
-        if (!administered_towns_ids.some(town_id => town_id != school.town._id))
-          throw ApiError.AccessDenied()
-      }
+      let school = await this.SchoolModel.findById(this.RolesService.getSchoolIdsFromRoles([role])[0])
+
+      if (!administered_towns_ids.some(town_id => town_id != school.town._id))
+        return false
+      
+      return true
     }
+
+    if (!user_roles.includes(role))
+      return false
+
+    return true
   }
 }
