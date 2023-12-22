04d224003f7018aa5c991c2daf3c9fc3f7f4611c
Yegor Knyazev <egorknyazev10@gmail.com>
2023-12-22 15:12:31 +0400

    reviewed school service

--- src/school/school.service.ts
+++ src/school/school.service.ts
@@ -9,7 +9,7 @@ export class SchoolService {
 
   hasAccess(roles: string[], school_id: string, town_id: string): boolean {
     return this.RolesService.isAdminOfSchool(roles, school_id) ||
-           this.RolesService.isAdminOfTown(roles, town_id.toString()) ||
+           this.RolesService.isAdminOfTown(roles, town_id) ||
            this.RolesService.isGlobalAdmin(roles)
   }
 }
